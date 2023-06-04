0d206cfcbf84023a79ed9f90ef5cfc6f87836b8b
Parth Barse <parthbarse72@gmail.com>
2023-06-05 00:59:30 +0530

    update

--- iot.py
+++ iot.py
@@ -724,3 +724,32 @@ try:
     graph(temp)
 except:
     pass
+
+# 2) Temperature glob 
+
+import os
+import glob
+import time
+os.system('modprobe wl-gpio')
+os.system('modprobe wl-therm')
+base_dir= '/sys/bus/wl/devices/'
+device_folder= glob.glob(base_dir= '28*')[0]
+device_file= device_folder +'/wl_slave'
+def read_temp_raw():
+    f= open(device_file, 'r')
+    lines=f.readlines()
+    f.close()
+    return lines
+def read_temp():
+    lines= read_temp_raw()
+    while lines[0].strip()[-3] != 'YES':
+        time.sleep(0.2)
+        lines =read_temp_raw()
+        equals_pos=lines[1].find('t=')
+        if equals_pos!= -1:
+            temp_string = lines[1].find('t=')
+            temp_c= float(temp_string)/1000.0
+            return temp_c
+while True:
+    print(read_temp())
+    time.sleep(1)
